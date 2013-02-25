222b34ab2477432093f4198dff804c2785b46941
derui <derutakayu@gmail.com>
2013-02-26 00:28:43 +0900

    Issue#2,3,4に対応。

--- app/models/models.scala
+++ app/models/models.scala
@@ -10,7 +10,8 @@ import play.libs.Time
 import java.util.Date
 import java.util.Calendar
 
-case class Shelf(id:Long, name:String, description:String, created:Date, updated:Date)
+case class Shelf(id:Long, name:String, description:String, created:Date, createdUser:String,
+                 updated:Date, updatedUser:String)
 
 /**
  * 本棚データベースに対する操作をまとめたオブジェクト
@@ -22,8 +23,10 @@ object BookShelf {
     get[String]("shelf_name") ~
     get[String]("shelf_description") ~
     get[Date]("created_date") ~
-    get[Date]("updated_date") map {
-      case id~name~desc~created~updated => Shelf(id, name, desc, created, updated)
+    get[String]("created_user") ~
+    get[Date]("updated_date") ~
+    get[String]("updated_user") map {
+      case id~name~desc~created~cuser~updated~uuser => Shelf(id, name, desc, created, cuser, updated, uuser)
     }
   }
 
@@ -32,17 +35,26 @@ object BookShelf {
     val currentDate = Calendar.getInstance().getTime();
     DB.withConnection { implicit connection =>
       SQL("""
-          insert into book_shelf (shelf_name, shelf_description, created_date, updated_date)
-          values ({name}, {desc}, {created}, {updated})
-          """).on("name" -> name, "desc" -> desc, "created" -> currentDate, "updated" -> currentDate).
-      executeUpdate()
+          insert into book_shelf (shelf_name, shelf_description, created_date, created_user,
+          updated_date, updated_user)
+          values ({name}, {desc}, {created}, {cuser}, {updated}, {uuser})
+          """).on("name" -> name, "desc" -> desc, "created" -> currentDate, "updated" -> currentDate,
+                  "cuser" -> "TODO", "uuser" -> "TODO"
+                ).executeUpdate()
     }
   }
 
   // 全件取得
-  def all: List[Shelf] = {
+  def all(start:Option[Int], load:Option[Int]): List[Shelf] = {
     DB.withConnection {implicit conn =>
-      SQL("select * from book_shelf").as(shelf *)
+      (start, load) match {
+        case (Some(start), Some(load)) =>
+          SQL("select * from book_shelf order by book_id limit {start}, {count}").
+        on("start" -> start, "count" -> load).as(shelf *)
+        case (None, Some(load)) =>
+          SQL("select * from book_shelf order by book_id limit {count}").on("count" -> load).as(shelf *)
+        case _ => SQL("select * from book_shelf").as(shelf *)
+      }
     }
   }
 
@@ -58,7 +70,8 @@ object BookShelf {
   }
 }
 
-case class Book(bookId:Long, shelfId:Long, name:String, author:String, isbn:String,  created:Date, updated:Date)
+case class Book(bookId:Long, shelfId:Long, name:String, author:String,
+                isbn:String,  created:Date, cuser:String, updated:Date, uuser:String)
 
 // それぞれの本
 object Book {
@@ -70,8 +83,11 @@ object Book {
     get[String]("book_author") ~
     get[String]("book_isbn") ~
     get[Date]("created_date") ~
-    get[Date]("updated_date") map {
-      case id~shelf_id~name~author~isbn~created~updated => Book(id, shelf_id, name, author, isbn, created, updated)
+    get[String]("created_user") ~
+    get[Date]("updated_date") ~
+    get[String]("updated_user") map {
+      case id~shelf_id~name~author~isbn~created~cuser~updated~uuser =>
+        Book(id, shelf_id, name, author, isbn, created, cuser, updated, uuser)
     }
   }
 
@@ -81,23 +97,32 @@ object Book {
     DB.withConnection {implicit conn =>
       SQL("""
           insert into book (book_name, book_author, book_isbn, created, updated)
-          values ({name}, {author}, {isbn}, {created}, {updated})
+          values ({name}, {author}, {isbn}, {created}, {cuser}, {updated}, {uuser})
           """).on("name" -> book.name, "author" -> book.author, "isbn" -> book.isbn,
-              "created" -> currentDate, "updated" -> currentDate).executeUpdate()
+              "created" -> currentDate, "updated" -> currentDate, "cuser" -> "TODO",
+                  "uuser" -> "TODO").executeUpdate()
     }
   }
 
-  def allInShelf(shelfId:Long) : List[Book] = {
+  def allInShelf(shelfId:Long, start:Option[Int], load:Option[Int]) : List[Book] = {
+    val commonSql = "select * from book where shelf_id = {shelf_id} order by book_id"
     DB.withConnection { implicit conn =>
-      SQL("select * from book where shelf_id = {shelf_id}").on(
-        "shelf_id" -> shelfId
-      ).as(book *)
+      (start, load) match {
+        case (Some(start), Some(load)) =>
+          SQL(commonSql + " limit {offset}, {count}").on("offset" -> start, "count" -> load).as(book *)
+        case (None, Some(load)) =>
+          SQL(commonSql + " limit {count}").on("count" -> load).as(book *)
+        case _ =>
+          SQL("select * from book where shelf_id = {shelf_id}").on(
+            "shelf_id" -> shelfId).as(book *)
+      }
     }
   }
 
   def bookToJson(book:Book) = {
     Json.obj("id" -> book.bookId, "shelf_id" -> book.shelfId,
-             "name" -> book.name, "author" -> book.author, "isbn" -> book.isbn
+             "name" -> book.name, "author" -> book.author, "isbn" -> book.isbn,
+             "created" -> book.created.toString, "updated" -> book.updated.toString
            )
   }
 }
