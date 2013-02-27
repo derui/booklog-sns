4396ba5bd4688f21066e29185a8e783ac09dade7
derui <derutakayu@gmail.com>
2013-02-27 18:08:24 +0900

    sencha touch用ファイルを削除

--- public/mock/app/model/BookShelfItem.js
+++
@@ -1,9 +0,0 @@
-Ext.define('Mock.model.BookShelfItem', {
-    extend: 'Ext.data.Model',
-    config: {
-        fields: [
-            {name: 'id', type:'number'},
-            {name: 'name', type:'string'}
-        ]
-    }
-});
