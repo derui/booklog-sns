82cb5b5b65b23057b97cc7a8d5cebd42b58423dc
kimura-m-29 <kimura.m.29@gmail.com>
2013-03-10 10:57:03 +0900

    ログアウトボタン追加 closed #28

--- public/javascripts/top.js
+++ public/javascripts/top.js
@@ -17,8 +17,30 @@ requirejs.config({
     }
 });
 
-requirejs(['lib/backbone', 'lib/pure', 'common'], function (Backbone) {
+function signinCallback(authResult) {
+    if (authResult['access_token']) {
+        // ログインボタンから認証すると、g-oauth-windowにWindowオブジェクトがセットされ
+        // JSON.stringify()でパースエラーとなるため、nullで初期化する
+        authResult['g-oauth-window'] = null;
+
+        $.ajax({
+            type: 'POST',
+            url: '/connect',
+            contentType: "application/json",
+            data: JSON.stringify(authResult),
+            success: function (response) {
+                var loginUserInfo = response.result[0];
+                $('#___signin_0').hide();
+                $('#signinArea').prepend('<span id="loginUserInfoArea"><img src="' + loginUserInfo.googlePublicProfilePhotoUrl + '" id="profilePhoto" /><span>' + loginUserInfo.googleDisplayName + '</span></span>');
+                $('#dissconectButton').removeClass('disabled');
+            }
+        });
+    }
+}
+
+requirejs(['lib/backbone', 'lib/pure', 'common', 'lib/zepto'], function (Backbone) {
     "use strict";
+
     // TODO モジュール化
     var BaseModel = Backbone.Model.extend({});
     var BaseCollection = Backbone.Collection.extend({});
@@ -72,4 +94,38 @@ requirejs(['lib/backbone', 'lib/pure', 'common'], function (Backbone) {
     });
 
     bookShelfList.fetch();
+
+    window.___gcfg = {
+        lang: 'ja',
+        parsetags: 'onload'
+    };
+
+    (function () {
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'https://apis.google.com/js/client:plusone.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+    })();
+
+    $(function () {
+        $('#dissconectButton').on('click', function () {
+            var $this = $(this);
+            
+            if ($this.hasClass('disabled')) {
+                return;
+            }
+
+            $.ajax({
+                type: 'GET',
+                url: '/disconnect',
+                success: function () {
+                    $('#loginUserInfoArea').remove();
+                    $('#___signin_0').css('display', 'inline-block').show();
+                    $this.addClass('disabled');
+                }
+            });
+        });
+    });
 });
