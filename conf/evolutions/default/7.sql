87901ef873dd3b7ead846d44fdd8171ea064dbbe
derui <derutakayu@gmail.com>
2013-03-26 08:53:36 +0900

    前に貼られていたuniqueインデックスを削除していなかったのでAlter文を追加。

--- conf/evolutions/default/7.sql
+++ conf/evolutions/default/7.sql
@@ -2,6 +2,8 @@
 
 alter table RentalInfo modify column RENTAL_USER_ID BIGINT NOT NULL;
 alter table RentalInfo modify column RENTAL_BOOK_ID BIGINT NOT NULL;
+alter table RentalInfo drop index RENTAL_BOOK_ID;
+alter table RentalInfo drop index RENTAL_USER_ID;
 alter table RentalInfo add unique rentaled_book_index (RENTAL_USER_ID, RENTAL_BOOK_ID);
 
 # --- !Downs
