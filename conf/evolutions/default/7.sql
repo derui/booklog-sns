ffda230582482dbfd0c481f739542d337a3e22b2
derui <derutakayu@gmail.com>
2013-03-26 08:45:31 +0900

    RentalInfoテーブルのUniqueインデックスの貼りかたが間違っていたので修正。

---
+++ conf/evolutions/default/7.sql
@@ -0,0 +1,10 @@
+# --- !Ups
+
+alter table RentalInfo modify column RENTAL_USER_ID BIGINT NOT NULL;
+alter table RentalInfo modify column RENTAL_BOOK_ID BIGINT NOT NULL;
+alter table RentalInfo add unique rentaled_book_index (RENTAL_USER_ID, RENTAL_BOOK_ID);
+
+# --- !Downs
+alter table RentalInfo modify column RENTAL_USER_ID BIGINT UNIQUE;
+alter table RentalInfo modify column RENTAL_BOOK_ID BIGINT UNIQUE;
+alter table RentalInfo drop index rentaled_book_index;
