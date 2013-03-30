1ea1e29a0f4e08852d466f5c04ee0b961de0bbe8
kimura-m-29 <kimura.m.29@gmail.com>
2013-03-30 23:16:11 +0900

    開発環境と本番環境でGoogle+のログイン時に使用するclient idを切り替えるよう修正しました closed #72

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -1,8 +1,24 @@
 requirejs.config({
-    baseUrl: '/javascripts'
+    baseUrl: '/javascripts',
+    shim: {
+        "lib/zepto": {
+            exports: "Zepto"
+        },
+        "lib/underscore": {
+            exports: "_"
+        },
+        "lib/pure": {
+            deps: ["lib/zepto"],
+            exports: "pure"
+        },
+        "lib/backbone": {
+            deps: ["lib/zepto", "lib/underscore"],
+            exports: "Backbone"
+        }
+    }
 });
 
-requirejs(['lib/zepto'], function () {
+requirejs(['lib/backbone', 'view', 'lib/pure', 'lib/zepto'], function (Backbone, View) {
     'use strict';
 
     window.signinCallback = function signinCallback(authResult) {
@@ -31,10 +47,43 @@ requirejs(['lib/zepto'], function () {
         parsetags: 'onload'
     };
 
-    var po = document.createElement('script');
-    po.type = 'text/javascript';
-    po.async = true;
-    po.src = 'https://apis.google.com/js/client:plusone.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(po, s);
+    var SigninButtonView = View.BaseView.extend({
+        el: '.g-signin',
+        initialize: function () {
+            var $signinSetting = this.$el;
+
+            $.getJSON('/api/detect_env', function (data) {
+                var clientid = '';
+                var environment = data.environment;
+
+                switch (environment) {
+                    case 'develop':
+                        clientid = '881447546058.apps.googleusercontent.com';
+                        break;
+                    case 'production':
+                        clientid = '881447546058-f3ref6v66g5tnkubcob4g6t1i35s78u4.apps.googleusercontent.com';
+                        break;
+                }
+
+                $signinSetting.attr('data-clientid', clientid);
+
+                // _.environment(); で環境が取得できるよう定義
+                _.mixin({
+                    'environment': function () {
+                        return environment;
+                    }
+                });
+            });
+
+            var po = document.createElement('script');
+            po.type = 'text/javascript';
+            po.async = true;
+            po.src = 'https://apis.google.com/js/client:plusone.js';
+            var s = document.getElementsByTagName('script')[0];
+            s.parentNode.insertBefore(po, s);
+        }
+    });
+
+    new SigninButtonView();
+
 });
